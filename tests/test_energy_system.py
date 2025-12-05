9198be0ec9204b6c55a426722990824a119ea656
m-rossini <marcos.tengelmann@gmail.com>
2025-12-05 22:27:03 +0000

    test: add tests for fat burning from deficit

--- tests/test_energy_system.py
+++ tests/test_energy_system.py
@@ -316,3 +316,148 @@ class TestApplyFatStorage:
         
         assert bean.size == initial_size
         assert bean.energy == initial_energy
+
+
+class TestApplyFatBurning:
+    """Tests for StandardEnergySystem.apply_fat_burning method."""
+
+    def test_apply_fat_burning_decreases_size_when_energy_below_baseline(self):
+        """When energy < energy_baseline, size should decrease (fat burned)."""
+        from beans.energy_system import StandardEnergySystem
+        
+        config = BeansConfig(
+            speed_min=-5, 
+            speed_max=5, 
+            initial_energy=100.0,
+            energy_baseline=50.0,
+            fat_burn_rate=0.1,
+            fat_to_energy_ratio=0.9
+        )
+        energy_system = StandardEnergySystem(config)
+        
+        genes = {
+            Gene.METABOLISM_SPEED: 0.5,
+            Gene.MAX_GENETIC_SPEED: 0.5,
+            Gene.FAT_ACCUMULATION: 0.5,
+            Gene.MAX_GENETIC_AGE: 0.5,
+        }
+        genotype = Genotype(genes=genes)
+        phenotype = create_phenotype(config, genotype)
+        phenotype.size = 15.0  # Has fat to burn
+        bean = Bean(config=config, id=1, sex=Sex.MALE, genotype=genotype, phenotype=phenotype)
+        bean._phenotype.energy = 30.0  # Below baseline of 50
+        
+        initial_size = bean.size
+        energy_system.apply_fat_burning(bean)
+        
+        assert bean.size < initial_size
+
+    def test_apply_fat_burning_increases_energy_by_conversion_ratio(self):
+        """Energy should increase by fat_burned * fat_to_energy_ratio when burning fat."""
+        from beans.energy_system import StandardEnergySystem
+        
+        config = BeansConfig(
+            speed_min=-5, 
+            speed_max=5, 
+            initial_energy=100.0,
+            energy_baseline=50.0,
+            fat_burn_rate=0.1,
+            fat_to_energy_ratio=0.8
+        )
+        energy_system = StandardEnergySystem(config)
+        
+        genes = {
+            Gene.METABOLISM_SPEED: 0.5,
+            Gene.MAX_GENETIC_SPEED: 0.5,
+            Gene.FAT_ACCUMULATION: 1.0,
+            Gene.MAX_GENETIC_AGE: 0.5,
+        }
+        genotype = Genotype(genes=genes)
+        phenotype = create_phenotype(config, genotype)
+        phenotype.size = 20.0  # Has fat to burn
+        bean = Bean(config=config, id=1, sex=Sex.MALE, genotype=genotype, phenotype=phenotype)
+        bean._phenotype.energy = 30.0  # 20 deficit below baseline of 50
+        
+        initial_energy = bean.energy
+        initial_size = bean.size
+        energy_system.apply_fat_burning(bean)
+        
+        # fat_burned = fat_burn_rate * FAT_ACCUMULATION * deficit = 0.1 * 1.0 * 20 = 2.0
+        # energy_gain = fat_burned * fat_to_energy_ratio = 2.0 * 0.8 = 1.6
+        size_decrease = initial_size - bean.size
+        energy_increase = bean.energy - initial_energy
+        
+        assert energy_increase == size_decrease * config.fat_to_energy_ratio
+
+    def test_higher_fat_accumulation_gene_increases_fat_burn(self):
+        """Higher FAT_ACCUMULATION gene should burn more fat from same deficit."""
+        from beans.energy_system import StandardEnergySystem
+        
+        config = BeansConfig(
+            speed_min=-5, 
+            speed_max=5, 
+            initial_energy=100.0,
+            energy_baseline=50.0,
+            fat_burn_rate=0.1,
+            fat_to_energy_ratio=0.9
+        )
+        energy_system = StandardEnergySystem(config)
+        
+        low_fat_genes = {
+            Gene.METABOLISM_SPEED: 0.5,
+            Gene.MAX_GENETIC_SPEED: 0.5,
+            Gene.FAT_ACCUMULATION: 0.2,
+            Gene.MAX_GENETIC_AGE: 0.5,
+        }
+        high_fat_genes = {
+            Gene.METABOLISM_SPEED: 0.5,
+            Gene.MAX_GENETIC_SPEED: 0.5,
+            Gene.FAT_ACCUMULATION: 0.8,
+            Gene.MAX_GENETIC_AGE: 0.5,
+        }
+        
+        low_genotype = Genotype(genes=low_fat_genes)
+        high_genotype = Genotype(genes=high_fat_genes)
+        
+        low_phenotype = create_phenotype(config, low_genotype)
+        high_phenotype = create_phenotype(config, high_genotype)
+        low_phenotype.size = 20.0
+        high_phenotype.size = 20.0
+        
+        low_bean = Bean(config=config, id=1, sex=Sex.MALE, genotype=low_genotype, phenotype=low_phenotype)
+        high_bean = Bean(config=config, id=2, sex=Sex.MALE, genotype=high_genotype, phenotype=high_phenotype)
+        low_bean._phenotype.energy = 30.0
+        high_bean._phenotype.energy = 30.0
+        
+        energy_system.apply_fat_burning(low_bean)
+        energy_system.apply_fat_burning(high_bean)
+        
+        # Higher FAT_ACCUMULATION should lose more size (burn more fat)
+        assert high_bean.size < low_bean.size
+
+    def test_apply_fat_burning_does_nothing_when_energy_at_or_above_baseline(self):
+        """No fat burning when energy >= energy_baseline."""
+        from beans.energy_system import StandardEnergySystem
+        
+        config = BeansConfig(
+            speed_min=-5, 
+            speed_max=5, 
+            initial_energy=100.0,
+            energy_baseline=50.0,
+            fat_burn_rate=0.1,
+            fat_to_energy_ratio=0.9
+        )
+        energy_system = StandardEnergySystem(config)
+        
+        genotype = create_random_genotype()
+        phenotype = create_phenotype(config, genotype)
+        phenotype.size = 15.0
+        bean = Bean(config=config, id=1, sex=Sex.MALE, genotype=genotype, phenotype=phenotype)
+        bean._phenotype.energy = 50.0  # At baseline
+        
+        initial_size = bean.size
+        initial_energy = bean.energy
+        energy_system.apply_fat_burning(bean)
+        
+        assert bean.size == initial_size
+        assert bean.energy == initial_energy
