1af5c0c3980fa97118026d5d48d9eb6f3429caf3
m-rossini <marcos.tengelmann@gmail.com>
2025-12-05 21:01:30 +0000

    test: add test for metabolism gene effect

--- tests/test_energy_system.py
+++ tests/test_energy_system.py
@@ -2,7 +2,7 @@ import pytest
 import logging
 from config.loader import BeansConfig
 from beans.bean import Bean, Sex
-from beans.genetics import create_random_genotype, create_phenotype
+from beans.genetics import create_random_genotype, create_phenotype, Genotype, Gene
 
 logger = logging.getLogger(__name__)
 
@@ -64,3 +64,52 @@ class TestApplyBasalMetabolism:
         energy_system.apply_basal_metabolism(bean)
         
         assert bean.energy == initial_energy - config.metabolism_base_burn
+
+    def test_higher_metabolism_gene_increases_burn_rate(self):
+        """Higher METABOLISM_SPEED gene value should increase basal burn rate."""
+        from beans.energy_system import EnergySystem
+        
+        config = BeansConfig(
+            speed_min=-5, 
+            speed_max=5, 
+            initial_energy=100.0,
+            initial_bean_size=10,
+            metabolism_base_burn=0.10
+        )
+        energy_system = EnergySystem(config)
+        
+        # Create two genotypes with different metabolism speeds
+        low_metabolism_genes = {
+            Gene.METABOLISM_SPEED: 0.0,
+            Gene.MAX_GENETIC_SPEED: 0.5,
+            Gene.FAT_ACCUMULATION: 0.5,
+            Gene.MAX_GENETIC_AGE: 0.5,
+        }
+        high_metabolism_genes = {
+            Gene.METABOLISM_SPEED: 1.0,
+            Gene.MAX_GENETIC_SPEED: 0.5,
+            Gene.FAT_ACCUMULATION: 0.5,
+            Gene.MAX_GENETIC_AGE: 0.5,
+        }
+        
+        low_genotype = Genotype(genes=low_metabolism_genes)
+        high_genotype = Genotype(genes=high_metabolism_genes)
+        
+        # Create phenotypes with same size to isolate metabolism effect
+        low_phenotype = create_phenotype(config, low_genotype)
+        high_phenotype = create_phenotype(config, high_genotype)
+        low_phenotype.size = 10.0
+        high_phenotype.size = 10.0
+        
+        low_bean = Bean(config=config, id=1, sex=Sex.MALE, genotype=low_genotype, phenotype=low_phenotype)
+        high_bean = Bean(config=config, id=2, sex=Sex.MALE, genotype=high_genotype, phenotype=high_phenotype)
+        
+        # Set same initial energy
+        low_bean._phenotype.energy = 100.0
+        high_bean._phenotype.energy = 100.0
+        
+        energy_system.apply_basal_metabolism(low_bean)
+        energy_system.apply_basal_metabolism(high_bean)
+        
+        # Higher metabolism should burn more energy
+        assert high_bean.energy < low_bean.energy
