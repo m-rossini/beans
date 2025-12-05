4b2e850d0ef08d704fd49774f1972de5543d0fd5
m-rossini <marcos.tengelmann@gmail.com>
2025-12-05 21:00:12 +0000

    test: add test for apply_basal_metabolism

--- tests/test_energy_system.py
+++ tests/test_energy_system.py
@@ -39,3 +39,28 @@ class TestApplyIntake:
         energy_system.apply_intake(bean, 10.0)
         
         assert bean.energy == initial_energy + 10.0
+
+
+class TestApplyBasalMetabolism:
+    """Tests for EnergySystem.apply_basal_metabolism method."""
+
+    def test_apply_basal_metabolism_decreases_energy_by_base_burn(self):
+        """apply_basal_metabolism should decrease energy by metabolism_base_burn."""
+        from beans.energy_system import EnergySystem
+        
+        config = BeansConfig(
+            speed_min=-5, 
+            speed_max=5, 
+            initial_energy=100.0,
+            metabolism_base_burn=0.05
+        )
+        energy_system = EnergySystem(config)
+        
+        genotype = create_random_genotype()
+        phenotype = create_phenotype(config, genotype)
+        bean = Bean(config=config, id=1, sex=Sex.MALE, genotype=genotype, phenotype=phenotype)
+        
+        initial_energy = bean.energy
+        energy_system.apply_basal_metabolism(bean)
+        
+        assert bean.energy == initial_energy - config.metabolism_base_burn
